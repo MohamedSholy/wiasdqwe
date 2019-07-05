2ad2602cfd0953d3e5807286d11d5328e7ea6a75
MohamedSholy <49501521+MohamedSholy@users.noreply.github.com>
2019-07-04 20:40:18 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`TEXT`,"http://twitch.tv/Dream")
+client.user.setGame(`‏اتحمّل المخلصين بأفعالهم السيئة اكثر من المزيفين بأفعالهم الطيبة`,"http://twitch.tv/Dream")
 client.user.setStatus("dnd")
 });
 
